fdef3ecb63636d6816ffc9e9696ee0c5b1f366a7
OkanSevken <okansevken20@gmail.com>
2024-06-14 23:31:52 +0300

    Updeted

---
+++ Core/StockTrackingApi.Application/Features/PartMovements/Command/CreatePartMovement/CreatePartMovementCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackingApi.Application.Features.PartMovements.Command.CreatePartMovement
+{
+    public class CreatePartMovementCommandRequest : IRequest<Unit>
+    {
+        public int PartId { get; set; }
+        public int WarehouseId { get; set; }
+        public int Amount { get; set; }
+        public string MovementType { get; set; } 
+        public string Description { get; set; }
+    }
+}
---
+++ Presentation/StockTrackingApi.Api/Controllers/PartMovementController.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockTrackingApi.Application.Features.PartMovements.Command.CreatePartMovement;
+
+namespace StockTrackingApi.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PartMovementController : ControllerBase
+    {
+        private readonly IMediator mediator;
+        public PartMovementController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreatePartMovements(CreatePartMovementCommandRequest request)
+        {
+            await mediator.Send(request);
+            return Ok();
+        }
+    }
+}
