8e4ff4cbbf2e49cabef1bddc60ae61157014629d
OkanSevken <okansevken20@gmail.com>
2024-06-18 13:06:14 +0300

    Ekleme yapıldı

---
+++ Core/StockTrackingApi.Application/Features/PartMovements/Queries/GetListPartMovementsFromInvoice/GetListPartMovementsFromInvoiceQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using StockTrackingApi.Application.Interfaces.UnitOfWorks;
+using StockTrackingApi.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackingApi.Application.Features.PartMovements.Queries.GetListPartMovementsFromInvoice
+{
+    public class GetListPartMovementsFromInvoiceQueryHandler : IRequestHandler<GetListPartMovementsFromInvoiceQueryRequest, IList<GetListPartMovementsFromInvoiceQueryResponse>>
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public GetListPartMovementsFromInvoiceQueryHandler(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public async Task<IList<GetListPartMovementsFromInvoiceQueryResponse>> Handle(GetListPartMovementsFromInvoiceQueryRequest request, CancellationToken cancellationToken)
+        {
+            var invoices = await unitOfWork.GetReadRepository<PartMovement>().GetAllAsync(x => x.Invoice == request.Invoice && x.IsActive == true && x.IsDeleted == false);
+            var parts = await unitOfWork.GetReadRepository<Part>().GetAllAsync(x => x.IsActive==true && x.IsDeleted==false);
+
+            List<GetListPartMovementsFromInvoiceQueryResponse> map = new List<GetListPartMovementsFromInvoiceQueryResponse>();
+
+            foreach (var invoice in invoices)
+            {
+                var partInvoice = parts.FirstOrDefault(x => x.Id == invoice.PartId);
+
+                map.Add(new GetListPartMovementsFromInvoiceQueryResponse
+                {
+                    PartId=partInvoice.Id,
+                    PartName = partInvoice.Name,
+                });
+            }
+            return map;
+        }
+    }
+}
---
+++ Core/StockTrackingApi.Application/Features/PartMovements/Queries/GetListPartMovementsFromInvoice/GetListPartMovementsFromInvoiceQueryRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrackingApi.Application.Features.PartMovements.Queries.GetListPartMovementsFromInvoice
+{
+    public class GetListPartMovementsFromInvoiceQueryRequest : IRequest<IList<GetListPartMovementsFromInvoiceQueryResponse>>
+    {
+        public bool Invoice { get; set; }
+    }
+}
